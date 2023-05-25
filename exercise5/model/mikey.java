23721e32e99d01eb6a87721e84bd55803ade8307
Joan Casimero <casimerojoan@gmail.com>
2023-05-25 10:37:12 +0800

    Add files via upload

---
+++ exercise5/model/mikey.java
@@ -0,0 +1,5 @@
+package model;
+
+public class mikey extends Them {
+
+}
---
+++ exercise5/model/them.java
@@ -0,0 +1,23 @@
+package model;
+
+class Them {
+
+    private String feature;
+    private String personality;
+
+    public String getFeature() {
+        return this.feature;
+    }
+
+    public void setFeature(String feature) {
+        this.feature = feature;
+    }
+
+    public String getPersonality() {
+        return this.personality;
+    }
+
+    public void setPersonality(String personality) {
+        this.personality = personality;
+    }
+}
